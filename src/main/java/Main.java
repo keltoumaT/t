fe149127476b396fa64697dc996928ae6ff9b077
keltoumaT <keltouma.tahi@gmail.com>
2022-02-16 08:06:54 -0500

    t' '

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,14 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
 
+
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,17 +22,20 @@ public class Main {
 
         UUID uuid = UUID.randomUUID();
         String tempFolder = "working-temp__" + uuid + "/";
-        String bucketName = "test-s3script";
+        String bucketName = "f-test-script";
+       // String pref = "level-t/";
+        List<String> prefs = new ArrayList<>();
+        prefs.add("level-t/");
+        prefs.add("level-e/");
 
         try {
-            AWSCredentials basicAWSCredentials = new BasicAWSCredentials("AKIAU657T2WKEHBDNIJ2", "iVFmaQ1Ru4ibX+1G4WbmMrn7t+tVzbvy6H8Kw9GY");
-            AmazonS3 amazonS3 = AmazonS3ClientBuilder
-                    .standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-                    .withRegion("ca-central-1")
-                    .build();
 
-            ListObjectsV2Request listObjectsV2Request = new ListObjectsV2Request().withBucketName(bucketName).withDelimiter("/").withPrefix("");
+
+            //  create array of all "folders" and loop through it from here (with prefix will contains the array value at current index)
+
+            for (String pref : prefs) {
+
+            ListObjectsV2Request listObjectsV2Request = new ListObjectsV2Request().withBucketName(bucketName).withDelimiter("/").withPrefix(pref);
             ListObjectsV2Result result;
 
             do {
@@ -40,29 +43,49 @@ public class Main {
 
                 for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
                     System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
-                    S3Object object = amazonS3.getObject(bucketName, objectSummary.getKey());
-                    InputStream inputStream = object.getObjectContent();
-                    // amazonS3.copyObject(bucketName, objectSummary.getKey(), bucketName, tempFolder + objectSummary.getKey());
-                    // CSVReader reader = new CSVReader(new InputStreamReader(inputStream, "UTF-8"));
-                    //String [] nextLine;
-                    //while(reader.readNext() != null){
-                    //  System.out.println();
-                    //}
-                    String s = inputStream.toString().replaceAll(",", "|");
-                    FileWriter writer = new FileWriter(object.getKey());
-                    BufferedReader reader1 = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                    String line;
-                    while ((line = reader1.readLine()) != null) {
-                        line.replaceAll(",", "|");
-                        writer.append(line);
-                        writer.append("\\n");
+                    // S3Object object = amazonS3.getObject(bucketName, objectSummary.getKey());
+                    //   InputStream inputStream = object.getObjectContent();
+                    List<String> commonPrefixes = result.getCommonPrefixes();
+                    for (String prefix : commonPrefixes) {
+
+                        String value = prefix.substring(pref.length());
+                        System.out.println(value);
+                        if (value.matches("\\d{4}-\\d{2}-\\d{2}\\/")) {
+                            System.out.println("value " + value);
+                            //then loop new request where prefix is this one
+                            ListObjectsV2Request listObjectsV2Request2 = new ListObjectsV2Request().withBucketName(bucketName).withDelimiter("/").withPrefix(prefix);
+                            ListObjectsV2Result result2;
+                            result2 = amazonS3.listObjectsV2(listObjectsV2Request2);
+                            for (S3ObjectSummary objectSummary1 : result2.getObjectSummaries()) {
+                                System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
+                                S3Object object1 = amazonS3.getObject(bucketName, objectSummary1.getKey());
+                                InputStream inputStream1 = object1.getObjectContent();
+
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                                StringWriter s = new StringWriter();
+                                CSVWriter writer = new CSVWriter(s, '|',
+                                        CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER, "\n");
+
+                                CSVReader reader = new CSVReader(new InputStreamReader(inputStream1, "UTF-8"));
+                                String[] nextLine;
+                                while ((nextLine = reader.readNext()) != null) {
+                                    writer.writeNext(nextLine);
+                                }
+                                ObjectMetadata metadata = new ObjectMetadata();
+
+                                //writer.flush();
+                                writer.close();
+                                String finalString = s.toString();
+                                metadata.setContentLength(finalString.getBytes().length);
+
+                                System.out.println("Actual data:- {} " + finalString);
+                                amazonS3.putObject(bucketName, object1.getKey(), new ByteArrayInputStream(finalString.getBytes()), metadata);
+
+                                System.out.println(String.valueOf(writer));
+                            }
+                        }
                     }
-                    System.out.println(s.toString());
-                    //File file = new File(writer.toString());
-
-                    writer.flush();
-                    writer.close();
-
                 }
                 // If there are more than maxKeys keys in the bucket, get a continuation token
                 // and list the next objects.
@@ -70,19 +93,19 @@ public class Main {
                 System.out.println("Next Continuation Token: " + token);
                 listObjectsV2Request.setContinuationToken(token);
             } while (result.isTruncated());
+        }
         } catch (
-                AmazonServiceException e) {
+                SdkClientException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
             e.printStackTrace();
-        } catch (
-                SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client
-            // couldn't parse the response from Amazon S3.
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
+        }// Amazon S3 couldn't be contacted for a response, or the client
+// couldn't parse the response from Amazon S3.
+        catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
             e.printStackTrace();
         }
     }
